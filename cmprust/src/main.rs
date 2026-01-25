16b3bf7ea51ea357511fc116a097ccdd51f7398c
antoinemeyer5 <antoine.meyer54@gmail.com>
2026-01-25 18:23:05 +0100

    feat: Draw line

--- cmprust/src/main.rs
+++ cmprust/src/main.rs
@@ -1,22 +1,47 @@
-use linrust::Linvec2;
-use ppmrust::Image;
-use ppmrust::Rgb;
+use linrust::{Linvec2, Linvec4};
+use ppmrust::{Image, Rgb};
 
 /****************************************************************** CONSTANTS */
 
 const BLACK: Rgb = Rgb { r: 0, g: 0, b: 0 };
-const WHITE: Rgb = Rgb { r: 255, g: 255, b: 255 };
+const WHITE: Rgb = Rgb {
+    r: 255,
+    g: 255,
+    b: 255,
+};
 
 /*************************************************************** MAIN PROGRAM */
 
 fn main() {
-    let mut image = Image::new(10, 15, BLACK);
+    let mut image = Image::new(100, 100, BLACK);
 
-    let a = Linvec2 { x: 0.0, y: 0.0 };
-    image.set_pixel(a.x as usize, a.y as usize, WHITE);
+    // Draw points
+    let points = [
+        Linvec2 { x: 0.0, y: 0.0 },
+        Linvec2 { x: 2.0, y: 2.0 },
+    ];
+    for point in points {
+        image.set_pixel(point.x as usize, point.y as usize, WHITE);
+    }
 
-    let b = Linvec2 { x: 1.0, y: 1.0 };
-    image.set_pixel(b.x as usize, b.y as usize, WHITE);
+    // Draw line
+    let line = Linvec4 {
+        x: 80.0,
+        y: 80.0,
+        z: 70.0,
+        w: 70.0,
+    };
+    image.draw_line(
+        line.x as usize,
+        line.y as usize,
+        line.z as usize,
+        line.w as usize,
+        WHITE,
+    );
 
-    image.save("image.ppm");
+    let saved = image.save("image.ppm");
+    match saved {
+        Ok(()) => println!("Well saved!"),
+        Err(error) => println!("Not saved because {}", error),
+    }
 }
