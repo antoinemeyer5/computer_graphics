5baaaccc69fb3b1322d2cba6d5fc85b6f497cd2a
antoinemeyer5 <antoine.meyer54@gmail.com>
2026-01-25 18:18:53 +0100

    feat: Add linvec4

--- linrust/src/lib.rs
+++ linrust/src/lib.rs
@@ -3,3 +3,6 @@ pub use linvec2::Linvec2;
 
 pub mod linvec3;
 pub use linvec3::Linvec3;
+
+pub mod linvec4;
+pub use linvec4::Linvec4;
---
+++ linrust/src/linvec4.rs
@@ -0,0 +1,47 @@
+/****************************************************************** STRUCTURE */
+
+#[derive(Debug)]
+pub struct Linvec4 {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+    pub w: f32,
+}
+
+/************************************************************* IMPLEMENTATION */
+
+impl Linvec4 {
+    pub fn add(self, other: Linvec4) -> Linvec4 {
+        Linvec4 {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+            w: self.w + other.w,
+        }
+    }
+}
+
+/****************************************************************** UNIT TEST */
+
+#[test]
+fn test_add() {
+    let a = Linvec4 {
+        x: 1.0,
+        y: 2.0,
+        z: 3.0,
+        w: 50.0,
+    };
+    let b = Linvec4 {
+        x: 4.0,
+        y: 5.0,
+        z: 6.0,
+        w: 51.0,
+    };
+
+    let result = a.add(b);
+
+    assert!(result.x == 5.0);
+    assert!(result.y == 7.0);
+    assert!(result.z == 9.0);
+    assert!(result.w == 101.0);
+}
