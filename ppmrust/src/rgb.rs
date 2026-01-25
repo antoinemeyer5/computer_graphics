fbf7f06b0072f6362c2d7fab79ddee39b5196e64
antoinemeyer5 <antoine.meyer54@gmail.com>
2026-01-25 22:53:41 +0100

    feat: Clone and Copy traits on Rgb

--- ppmrust/src/rgb.rs
+++ ppmrust/src/rgb.rs
@@ -1,6 +1,11 @@
 /****************************************************************** STRUCTURE */
 
-#[derive(Clone)]
+// Clone: allows explicit copying of a type’s value.
+// - This method is responsible for creating a new copy of the value.
+// Copy: signifies that a type can be copied bit by bit.
+// - It simply marks a type as being eligible for bitwise copying.
+
+#[derive(Clone, Copy)]
 pub struct Rgb {
     pub r: usize,
     pub g: usize,
