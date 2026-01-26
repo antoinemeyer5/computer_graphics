a89b7ee1649ea18688067808806b25f32abf593b
antoinemeyer5 <antoine.meyer54@gmail.com>
2026-01-26 17:51:38 +0100

    feat: Use new Line

--- cmprust/src/main.rs
+++ cmprust/src/main.rs
@@ -18,20 +18,25 @@ fn main() {
     let mut image = Image::new(40, 50, BLACK);
 
     // Draw points
-    let points = [Point::new(0.0, 0.0), Point::new(2.0, 2.0)];
+    let points = [Point::from_vec((0.0, 0.0)), Point::new(2.0, 2.0)];
     for point in points {
-        image.set_pixel(point.pos.x as usize, point.pos.y as usize, RED);
+        image.set_pixel(point.x() as usize, point.y() as usize, RED);
     }
 
-    // Draw line
-    let line = Line::new(12.0, 15.0, 25.0, 45.0);
-    image.draw_line(
-        line.start().x as usize,
-        line.data.y as usize,
-        line.data.z as usize,
-        line.data.w as usize,
-        WHITE,
-    );
+    // Draw lines
+    let lines = [
+        Line::from_vec((12.0, 15.0, 25.0, 45.0)),
+        Line::from_points(Point::new(10.0, 0.0), Point::new(20.0, 2.0)),
+    ];
+    for line in lines {
+        image.draw_line(
+            line.start().x() as usize,
+            line.start().y() as usize,
+            line.end().x() as usize,
+            line.end().y() as usize,
+            WHITE,
+        );
+    }
 
     // Draw triangle
     let triangle = Triangle::new(8.0, 8.0, 27.0, 3.0, 26.0, 17.0);
