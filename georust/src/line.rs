9857d5a517c8e125f4ade343ed59082a89a7f1f7
antoinemeyer5 <antoine.meyer54@gmail.com>
2026-01-26 17:34:08 +0100

    feat: Create Point and Line from vec

--- georust/src/line.rs
+++ georust/src/line.rs
@@ -3,7 +3,7 @@ use linrust::{Linvec2, Linvec4};
 /****************************************************************** STRUCTURE */
 
 pub struct Line {
-    pub data: Linvec4,
+    data: Linvec4,
 }
 
 /************************************************************* IMPLEMENTATION */
@@ -11,6 +11,10 @@ pub struct Line {
 impl Line {
     pub fn end(&self) -> Linvec2 {
         Linvec2::new(self.data.z, self.data.w)
+    }
+
+    pub fn from_vec(v: (f32, f32, f32, f32)) -> Self {
+        Self::new(v.0, v.1, v.2, v.3)
     }
 
     pub fn new(x1: f32, y1: f32, x2: f32, y2: f32) -> Self {
--- georust/src/point.rs
+++ georust/src/point.rs
@@ -3,15 +3,27 @@ use linrust::Linvec2;
 /****************************************************************** STRUCTURE */
 
 pub struct Point {
-    pub pos: Linvec2,
+    pos: Linvec2,
 }
 
 /************************************************************* IMPLEMENTATION */
 
 impl Point {
+    pub fn from_vec(v: (f32, f32)) -> Self {
+        Self::new(v.0, v.1)
+    }
+
     pub fn new(x: f32, y: f32) -> Self {
         Self {
             pos: Linvec2::new(x, y),
         }
+    }
+
+    pub fn x(&self) -> f32 {
+        self.pos.x
+    }
+
+    pub fn y(&self) -> f32 {
+        self.pos.y
     }
 }
