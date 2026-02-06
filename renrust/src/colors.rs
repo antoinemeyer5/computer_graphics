82b21181efe62a33d6057ef88e654cc698743335
antoinemeyer5 <antoine.meyer54@gmail.com>
2026-02-06 23:39:43 +0100

    feat: Format

--- renrust/src/colors.rs
+++ renrust/src/colors.rs
@@ -6,7 +6,7 @@ pub mod colors {
     use super::Rgb;
 
     pub const BLACK: Rgb = Rgb { r: 0, g: 0, b: 0 };
-    pub const BLUE: Rgb = Rgb {r: 0, g: 0, b: 255};
+    pub const BLUE: Rgb = Rgb { r: 0, g: 0, b: 255 };
     pub const WHITE: Rgb = Rgb {
         r: 255,
         g: 255,
