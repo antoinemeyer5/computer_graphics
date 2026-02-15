1c6932f5196909c9d711a2d6942c47ba1cc2d1f7
antoinemeyer5 <antoine.meyer54@gmail.com>
2026-02-15 16:38:33 +0100

    feat: Use Oval in cmprust

--- cmprust/src/main.rs
+++ cmprust/src/main.rs
@@ -1,6 +1,6 @@
-use georust::{Line, Point, Rectangle, Triangle};
+use georust::{Line, Oval, Point, Rectangle, Triangle};
 use ppmrust::Image;
-use renrust::{colors::*, draw_line, draw_point, draw_rectangle, draw_triangle};
+use renrust::{colors::*, draw_line, draw_point, draw_rectangle, draw_triangle, render::draw_oval};
 
 /*************************************************************** MAIN PROGRAM */
 
@@ -10,7 +10,7 @@ fn main() {
 
     // Draw points
     let points = [
-        Point::from_vec((0.0, 0.0)),
+        Point::from_vec((75.0, 75.0)),
         Point::new(2.0, 2.0),
         Point::new(39.0, 49.0),
     ];
@@ -21,7 +21,7 @@ fn main() {
     // Draw lines
     let lines = [
         Line::from_vec((12.0, 15.0, 25.0, 45.0)),
-        Line::from_points(Point::new(10.0, 0.0), Point::new(20.0, 2.0)),
+        Line::from_points(Point::new(10.0, 9.0), Point::new(20.0, 2.0)),
     ];
     for line in lines {
         draw_line(&mut image, line, colors::WHITE);
@@ -29,9 +29,9 @@ fn main() {
 
     // Draw triangle
     let triangle = Triangle::from_points(
-        Point::new(8.0, 8.0),
-        Point::new(27.0, 3.0),
-        Point::new(26.0, 17.0),
+        Point::new(18.0, 18.0),
+        Point::new(47.0, 13.0),
+        Point::new(36.0, 27.0),
     );
     draw_triangle(&mut image, triangle, colors::GREEN);
 
@@ -42,18 +42,27 @@ fn main() {
     // Draw rectangles
     let rectangles = [
         Rectangle::new(Point::new(10.0, 80.0), 54.0, 25.0, 0.0),
-        Rectangle::new(Point::new(87.0, 65.0), 14.0, 76.0, 33.0),
+        Rectangle::new(Point::new(98.0, 65.0), 14.0, 76.0, 33.0),
     ];
     for rectangle in rectangles {
-        draw_rectangle(&mut image, rectangle, colors::YELLOW);
+        draw_rectangle(&mut image, rectangle, colors::BLUE);
     }
 
-    // TODO: Draw circles
-    // TODO: Draw polygons (store a Vec<Point>)
+    // Draw ovals/ellipses
+    let ovals = [
+        Oval::new(Point::new(150.0, 90.0), 3.4, 18.9),
+        Oval::new(Point::new(130.0, 30.0), 10.0, 5.0),
+    ];
+    for oval in ovals {
+        draw_oval(&mut image, oval, colors::YELLOW);
+    }
 
-    // TODO: IDEA:
-    // TODO: - Gradient color
-    // TODO: - Fill shapes
+    // Draw circle
+    let circle = Oval::circle(Point::new(170.0, 130.0), 19.0);
+    draw_oval(&mut image, circle, colors::YELLOW);
+
+    // Draw polygons (store a Vec<Point>)
+    // TODO
 
     // Save image
     let saved = image.save("image.ppm");
